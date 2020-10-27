034419166958dac1190b73c2565b15eacfc56b56
Romchik K <56064449+custer-debug@users.noreply.github.com>
2020-10-27 12:56:47 +0300

    Функции добавлены в отдельные файлы. Добавлен переход на страницу авторизации после удаления страницы пользователя

--- MainServerFile.go
+++ MainServerFile.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
 	"html/template"
 	"log"
 	"net/http"
@@ -22,71 +21,10 @@ type User struct {
 var database *sql.DB
 var GlobalUser = new(User)
 
-//Функция которая отвечает за создание пользователя и запись в БД, полученных со страницы данных
-func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		_ = r.ParseForm()
-
-		firstname := r.FormValue("firstname")
-		lastname := r.FormValue("lastname")
-		age := r.FormValue("age")
-		login := r.FormValue("login")
-		password := r.FormValue("password")
-
-		fmt.Printf("Name:%s\nSurname:%s\n", firstname, lastname)
-		var _, err = database.Exec(
-			"insert into user.dataofusers (firstname, lastname, age, login, password) values (?,?,?,?,?)",
-			firstname, lastname, age, login, password)
-		if err != nil {
-			log.Println(err)
-		}
-		http.Redirect(w, r, "/auth/", 301)
-	} else {
-
-		http.ServeFile(w, r, "html/PageRegister.html")
-	}
-
-}
-
 //Главная страница, которая открывается после авторизации и при нажатии на значок Packy
 func MainPage(w http.ResponseWriter, _ *http.Request) {
 	tmpl, _ := template.ParseFiles("html/SuccessfullEnter.html")
 	_ = tmpl.Execute(w, GlobalUser)
-}
-
-//Функция которая отвечает за вход
-func LoginFunc(w http.ResponseWriter, r *http.Request) {
-	var u = new(User)
-
-	if r.Method == "POST" {
-
-		_ = r.ParseForm()
-		login := r.FormValue("login")
-		Password := r.FormValue("pass")
-		rows := database.QueryRow("select * from dataofusers where login = ? and password = ?;", login, Password)
-
-		_ = rows.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Age, &u.Login, &u.Password)
-		GlobalUser = u
-		if len(u.FirstName) > 0 && len(u.LastName) > 0 {
-			fmt.Printf("First Name:\t%s\nLast Name:\t%s\n",
-				u.FirstName, u.LastName)
-
-			http.Redirect(w, r, "/main_page/", 301)
-			u = nil
-		} else {
-
-			http.Redirect(w, r, "/auth/", 301)
-		}
-	} else {
-
-		http.ServeFile(w, r, "html/PageAut.html")
-	}
-
-}
-
-func LogoutFunc(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/auth/", 301)
-	GlobalUser = nil
 }
 
 //Функция которая отвечает за редактирование данных из БД
@@ -104,7 +42,7 @@ func SettingHandleFunc(w http.ResponseWriter, r *http.Request) {
 				log.Println(err)
 			}
 			fmt.Printf("Delete user:%s %s\n", GlobalUser.FirstName, GlobalUser.LastName)
-			LogoutFunc(w, r)
+			//LogoutFunc(w, r)
 			GlobalUser = nil
 
 		} else {
@@ -139,15 +77,16 @@ func main() {
 	database = db
 	defer db.Close()
 
-	router := mux.NewRouter()
-	router.HandleFunc("/auth/", LoginFunc)
-	router.HandleFunc("/logout/", LogoutFunc)
+	//var router = mux.NewRouter()
 
-	router.HandleFunc("/create/", CreateUserHandler)
-	router.HandleFunc("/settings/", SettingHandleFunc)
-	router.HandleFunc("/main_page/", MainPage)
+	http.HandleFunc("/auth/", LoginFunc)
+	http.HandleFunc("/logout/", LogoutFunc)
 
-	http.Handle("/", router)
+	http.HandleFunc("/create/", CreateUserHandler)
+	http.HandleFunc("/settings/", SettingHandleFunc)
+	http.HandleFunc("/main_page/", MainPage)
+
+	//	http.Handle("/", router)
 	http.Handle("/CSS/", http.StripPrefix("/CSS/", http.FileServer(http.Dir("./CSS/"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js/"))))
 
