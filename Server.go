d94645c806d036d025c334eb530f6ea1c568cbff
Romchik K <56064449+custer-debug@users.noreply.github.com>
2020-11-15 00:47:21 +0300

    Fix changing user data

--- Server.go
+++ Server.go
@@ -5,6 +5,7 @@ import (
 	iof "custer-debug/in-out-function"
 	"database/sql"
 	json2 "encoding/json"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"log"
@@ -21,21 +22,48 @@ func getCookies(ctx *fiber.Ctx) *iof.User {
 }
 
 func GetWelcomeHandler(ctx *fiber.Ctx) error {
-
 	var user = getCookies(ctx)
-
+	fmt.Println(user)
 	return ctx.Render("./html/Welcome.html", user)
 }
 
 func HandlerGetSettings(ctx *fiber.Ctx) error {
-
 	var user = getCookies(ctx)
 
 	return ctx.Render("./html/Settings.html", user)
 }
 
+func HandlerPostSettings(ctx *fiber.Ctx) error {
+	var user = getCookies(ctx)
+
+	user.Firstname = ctx.FormValue("NewName")
+	user.Lastname = ctx.FormValue("NewSurname")
+
+	json, _ := json2.Marshal(user)
+	ctx.Cookie(&fiber.Cookie{
+		Name:  "user",
+		Value: string(json),
+	})
+
+	db, _ := sql.Open("mysql",
+		"root:Systemofadown2011@tcp(:8080)/user")
+	defer db.Close()
+	_, err := db.Exec("update dataofusers set firstname = ?,lastname = ? where ID = ?",
+		user.Firstname,
+		user.Lastname,
+		user.ID,
+	)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return ctx.Redirect("/welcome")
+}
+
 func DeleteUser(ctx *fiber.Ctx) error {
-	db, _ := sql.Open("mysql", "root:Systemofadown2011@tcp(:8080)/user")
+	db, _ := sql.Open("mysql",
+		"root:Systemofadown2011@tcp(:8080)/user")
 	defer db.Close()
 	var user = getCookies(ctx)
 	_, err := db.Exec("delete from dataofusers where id = ?", user.ID)
@@ -61,6 +89,7 @@ func MainHandler(app *fiber.App) {
 	app.Post("/create", crus.PostCreateHandler)
 
 	app.Get("/settings", HandlerGetSettings)
+	app.Post("/settings", HandlerPostSettings)
 
 	app.Get("/delete", DeleteUser)
 
